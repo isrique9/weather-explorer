0098f8b16668bf339aca10287d3fd8f317dd87b9
isrique9 <henriqueisrael790@gmail.com>
2026-05-04 15:47:29 -0300

    chore: :wrench: Caminho do projeto atualizado.

--- weather-explorer/vite.config.js
+++ weather-explorer/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/weather-explorer-app',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
